8437bfef2c3de3d9a171f1be31636a7745ad4c26
shorcicka <sergej.horcicka@gmail.com>
2018-11-19 16:54:53 +0100

    SHO: added footer, header, progressBar, favicon, project structure redes

--- semerad-autoopravy-web/src/main/web/src/app/app.component.ts
+++ semerad-autoopravy-web/src/main/web/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ProgressbarConfig } from 'ngx-bootstrap/progressbar';
+import { ScrollEvent } from 'ngx-scroll-event';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: ProgressbarConfig, useFactory: getProgressbarConfig }]
 })
 export class AppComponent {
-  title = 'web';
+  public max = 100;
+  public progress;
+
+  public changeProgress(event: ScrollEvent, value: number): void {
+
+    const docH = document.body.clientHeight;
+    console.log('docH', docH);
+    const scrT = document.documentElement.scrollTop;
+    console.log('scrT', scrT);
+    const winH = window.innerHeight;
+    console.log('winH', winH);
+    const scrP = (scrT / (docH - winH));
+    console.log('scrP', scrP);
+    const scrollPercentRounded = Math.round(Number((scrP * 100).toFixed(1)));
+    console.log('scrollPercentRounded', scrollPercentRounded);
+    this.progress = scrollPercentRounded;
+  }
+}
+
+export function getProgressbarConfig(): ProgressbarConfig {
+  return Object.assign(new ProgressbarConfig(), { animate: true, striped: true, max: 100 });
 }
--- semerad-autoopravy-web/src/main/web/src/app/app.module.ts
+++ semerad-autoopravy-web/src/main/web/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from '@angular/common';
+import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { ScrollEventModule } from 'ngx-scroll-event';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    ScrollEventModule,
+    ProgressbarModule.forRoot()
   ],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/' },
