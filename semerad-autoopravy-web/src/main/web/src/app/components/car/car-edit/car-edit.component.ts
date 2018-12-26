5b2c626446520e167920afbb95f8903293a91bf6
shorcicka <sergej.horcicka@gmail.com>
2018-12-27 00:02:41 +0100

    SHO: car edit/add form fixed

--- semerad-autoopravy-web/src/main/web/src/app/components/car/car-edit/car-edit.component.ts
+++ semerad-autoopravy-web/src/main/web/src/app/components/car/car-edit/car-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CarModel } from 'src/app/models/CarModel';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {first} from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 import { CarService } from '../car.service';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -20,7 +20,7 @@ export class CarEditComponent implements OnInit {
   public repairsArray: any[] = [];
 
   constructor(private formBuilder: FormBuilder, private router: Router,
-     private carService: CarService, private activeRoute: ActivatedRoute) { }
+    private carService: CarService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
     const queryParams = this.activeRoute.snapshot.queryParams;
@@ -45,31 +45,37 @@ export class CarEditComponent implements OnInit {
     });
 
     this.carService.getCarById(routeParams.id)
-      .subscribe( data => {
+      .subscribe(data => {
         console.log('Data readed to set editForm' + JSON.stringify(data));
         this.editForm.setValue(data);
       });
 
-    }
+  }
 
-    onSubmit() {
-      Swal({
-        position: 'center',
-        type: 'success',
-        title: 'Zaznam ulozen',
-        showConfirmButton: false,
-        timer: 1500
-      });
-
-      this.carService.updateCar(this.editForm.value)
-        .pipe(first())
-        .subscribe(
-          data => {
-            this.router.navigate(['cars']);
-          },
-          error => {
-            alert(error);
+  onSubmit() {
+    this.carService.updateCar(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['cars']);
+        },
+        error => {
+          console.log('Error while saving edited car: ' + JSON.stringify(error));
+          Swal({
+            type: 'error',
+            title: 'Chyba! ' + error.status,
+            text: 'Zprava: ' + error.message,
+            footer: '<a>Zkuste ulozit znovu</a>'
           });
-    }
+        });
+
+    Swal({
+      position: 'center',
+      type: 'success',
+      title: 'Zaznam ulozen',
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
 
 }
